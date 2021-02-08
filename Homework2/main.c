5764df829161548570bf91bd703a0150b2ac6b3a
mmmkie <setupmm.mike@gmail.com>
2021-02-08 17:38:01 -0500

    Se agrega primera version de del menú principal y sus salidas

--- Homework2/main.c
+++ Homework2/main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 
+void main_menu();
+void movie_menu();
+void sudo();
+
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    main_menu();
+
 }
+
+void main_menu() {
+    char main[0];
+    puts(" @@@@@ Bienvenido al menú del cinema @@@@@ ");
+    puts(" En qué modo deseas acceder:");
+    puts("0. Sudo");
+    puts("1. Customer");
+    scanf("%s",main);
+    switch (main[0]) {
+    case '0':
+        sudo();
+        break;
+    case '1':
+        movie_menu();
+        break;
+    default:
+        puts("Incorrecto, ingresa de nuevo: ");
+        main_menu();
+    }
+
+}
+
+void movie_menu(){
+    puts("hi");
+}
+
+void sudo(){
+    printf("ingrsas sudo");
+}
+
+/*
+    char tictactoe[10][13];
+    int x,y;
+
+    for(x=0;x<10;x++)
+        for(y=0;y<13;y++)
+            tictactoe[x][y] = '#';
+
+    puts("Tic Tac Toe");
+    for(x=0;x<10;x++)
+    {
+        for(y=0;y<13;y++)
+            printf(" %c ",tictactoe[x][y]);
+        putchar('\n');
+    }
+
+    return(0);
+ */
